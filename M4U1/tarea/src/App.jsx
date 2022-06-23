dce73af93819b694248dca1dd3f16af8a5c26902
Lucas-ez <lucasezequiel237@gmail.com>
2022-06-23 20:32:24 -0300

    tarea1 node

---
+++ M4U1/tarea/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <a href="https://www.google.com">Google</a>
+    </div>
+  );
+}
+
+export default App;
